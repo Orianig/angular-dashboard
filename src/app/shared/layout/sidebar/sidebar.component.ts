e7a8d6dcddb254380be6d8f93cc50b81152d3e73
orianig <oriana.ig016@gmail.com>
2023-08-04 12:31:13 +0200

    feat: add submenu to logic function

--- src/app/shared/layout/sidebar/sidebar.component.ts
+++ src/app/shared/layout/sidebar/sidebar.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { ThemeService } from 'src/app/core/services/theme.service';
+import { MENU_LIST } from './menu.constant';
+import { IMenuItem } from 'src/app/core/models/menu.interface';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
   showSubmenu = false;
+  public menuItemsList: IMenuItem[] = MENU_LIST;
+  public subMenuItemsList: IMenuItem[] = [];
 
   constructor(private themeService: ThemeService) {}
-  
+
   toggleTheme() {
     const currentTheme = this.themeService.theme;
-    this.themeService.theme = currentTheme == 'dark' ? 'light' : 'dark'
+    this.themeService.theme = currentTheme == 'dark' ? 'light' : 'dark';
     // Otro camino, yo prefiero usar el operador ternario
     // if (currentTheme == 'dark') {
     //   this.themeService.theme = 'light'
@@ -25,6 +29,10 @@ export class SidebarComponent {
   toggleSubmenu(section: string) {
     if (section !== 'dashboard') {
       this.showSubmenu = !this.showSubmenu;
+      // Si se muestra el submenu, actualizamos los elementos activos del submenu
+      this.subMenuItemsList = this.showSubmenu
+        ? this.menuItemsList.find((item) => item.name === section)?.items || []
+        : [];
     }
   }
 }
