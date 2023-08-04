455c5a1e547f3be20a8babc70600beb59f18e454
orianig <oriana.ig016@gmail.com>
2023-08-04 12:29:27 +0200

    feat: add menu & submenu items structure - elements

---
+++ src/app/shared/layout/sidebar/menu.constant.ts
@@ -0,0 +1,28 @@
+import { IMenuItem } from '../../../core/models/menu.interface';
+
+export const MENU_LIST: IMenuItem[] = [
+  {
+    name: 'Clientes',
+    active: false,
+    icon: 'fa-solid fa-users',
+    items: [
+      { name: 'Ver Clientes', route: '/clientes', active: false },
+      { name: 'Agregar Cliente', route: '/clientes/agregar', active: false },
+      { name: 'Editar Cliente', route: '/clientes/editar', active: false },
+    ],
+  },
+  {
+    name: 'Proveedores',
+    active: false,
+    icon: 'fa-solid fa-truck-moving',
+    items: [
+      { name: 'Ver Proveedores', route: '/proveedores', active: false },
+      {
+        name: 'Agregar Proveedor',
+        route: '/proveedores/agregar',
+        active: false,
+      },
+      { name: 'Editar Proveedor', route: '/proveedores/editar', active: false },
+    ],
+  },
+];
